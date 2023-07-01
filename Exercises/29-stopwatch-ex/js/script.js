dda123e8c9860db498bd82b6fb9c2f38fb3f1726
Ramon Farias <114533455+ramonfarias1@users.noreply.github.com>
2023-07-01 18:04:16 -0300

    update

--- Exercises/29-stopwatch-ex/js/script.js
+++ Exercises/29-stopwatch-ex/js/script.js
@@ -19,7 +19,11 @@ function timer() {
         return `${zeroAEsquerda(hour)}:${zeroAEsquerda(min)}:${zeroAEsquerda(seg)}`;
     };
 
+    const startButton = document.querySelector('span.start-button');
+    const pauseButton = document.querySelector('span.pause-button');
+    const resetButton = document.querySelector('span.reset-button');
     const tagH1 = document.querySelector('h1');
+    const title = document.querySelector('.title');
 
     let hour = 0;
     let min = 0;
@@ -31,17 +35,24 @@ function timer() {
         const element = event.target;
         if (element.classList.contains('start-button')) {
             clearInterval(timer);
-            tagH1.classList.remove('stopped');
+            tagH1.classList.remove('paused');
+            startButton.classList.add('hidden');
+            pauseButton.classList.remove('hidden');
+            resetButton.classList.remove('hidden');
 
             timer = setInterval(function () {
                 tagH1.innerText = getTimer();
+                title.innerText = tagH1.innerText + ' - Stopwatch';
+
             }, 1000);
         };
 
-        if (element.classList.contains('stop-button')) {
+        if (element.classList.contains('pause-button')) {
             clearInterval(timer);
 
-            tagH1.classList.add('stopped');
+            tagH1.classList.add('paused');
+            startButton.classList.remove('hidden');
+            pauseButton.classList.add('hidden');
         };
 
         if (element.classList.contains('reset-button')) {
@@ -52,7 +63,10 @@ function timer() {
             seg = 0;
 
             tagH1.innerText = `00:00:00`;
-            tagH1.classList.remove('stopped');
+            tagH1.classList.remove('paused');
+            startButton.classList.remove('hidden');
+            pauseButton.classList.add('hidden');
+            resetButton.classList.add('hidden');
         };
     });
 };
