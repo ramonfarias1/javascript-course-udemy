2fd3446a049a8556fe7a8757ff5bfcb88cba9c62
Ramon Farias <114533455+ramonfarias1@users.noreply.github.com>
2023-08-17 20:09:17 -0300

    update

--- Exercises/33-JavaScript-POO-ex/validacao-formulario/js/script.js
+++ Exercises/33-JavaScript-POO-ex/validacao-formulario/js/script.js
@@ -1,32 +1,74 @@
 class Form {
-    constructor(nome, sobrenome, cpf, usuario, senha, senhaRepetida) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.cpf = cpf.replaceAll('.', '').replace('-', '');
-        this.usuario = usuario;
-        this.senha = senha;
-        this.senhaRepetida = senhaRepetida;
+    constructor() {
+        this.inputNome = document.querySelector('input#nome');
+        this.inputSobrenome = document.querySelector('input#sobrenome');
+        this.inputCpf = document.querySelector('input#cpf');
+        this.inputUsuario = document.querySelector('input#usuario');
+        this.inputSenha = document.querySelector('input#senha');
+        this.inputRepetirSenha = document.querySelector('input#repetir-senha');
     };
 
-    validarNome() {
-        const pErroNome = document.querySelector('p#erro-nome');
+    validaForm() {
+        this.validaNome(ValidaNomeCallBack);
 
-        if (!this.nome) {
-            pErroNome.innerText
+        function ValidaNomeCallBack() {
+            this.validaSobrenome(validaSobrenomeCallBack);
         };
+
+        function validaSobrenomeCallBack() {
+            this.validaCpf(validaCpfCallBack);
+        };
+
     };
+
+    validaNome(callback) {
+        const divErrosNome = document.querySelector('div#erros-nome');
+        divErrosNome.innerHTML = '';
+        divErrosNome.style.display = 'none';
+
+        if (!this.inputNome.value) {
+            divErrosNome.innerHTML += '<p>O campo "nome" é obrigatório!</p>';
+            divErrosNome.style.display = 'block';
+            if (callback) callback();
+        };
+
+        if (!isNaN(this.inputNome.value)) {
+            divErrosNome.innerHTML += '<p>Nome Inválido!</p>';
+            divErrosNome.style.display = 'block';
+        };
+
+        if (callback) callback();
+    };
+
+    validaSobrenome(callback) {
+        const divErrosSobrenome = document.querySelector('div#erros-sobrenome');
+        divErrosSobrenome.innerHTML = '';
+        divErrosSobrenome.style.display = 'none';
+
+        if (!this.inputSobrenome.value) {
+            divErrosSobrenome.innerHTML += '<p>O campo "Sobrenome" é obrigatório!</p>';
+            divErrosSobrenome.style.display = 'block';
+            if (callback) callback();
+        };
+
+        if (!isNaN(this.inputNome.value)) {
+            divErrosSobrenome.innerHTML += '<p>Sobrenome Inválido!</p>';
+            divErrosSobrenome.style.display = 'block';
+        };
+        
+        if (callback) callback();
+    };
+
+    validaCpf(callback) {
+        console.log('test');
+    };
+
 };
 
 const botaoEnviar = document.querySelector('input#enviar');
-
 botaoEnviar.addEventListener('click', ()=> {
-    const inputNome = document.querySelector('input#nome');
-    const inputSobrenome = document.querySelector('input#sobrenome');
-    const inputCPF = document.querySelector('input#cpf');
-    const inputUsuario = document.querySelector('input#usuario');
-    const inputSenha = document.querySelector('input#senha');
-    const inputRepetirSenha = document.querySelector('input#repetir-senha');
 
-    const form = new Form(inputNome.value, inputSobrenome.value, inputCPF.value, inputUsuario.value, inputSenha.value, inputRepetirSenha.value);
-    form.validarNome();
+    const form = new Form();
+    form.validaForm();
+
 });
