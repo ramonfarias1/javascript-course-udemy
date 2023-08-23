f5885d74fb2014c2919e6670fa98d7d88f2e2829
Ramon Farias <114533455+ramonfarias1@users.noreply.github.com>
2023-08-23 20:48:54 -0300

    update

--- Exercises/33-JavaScript-POO-ex/validacao-formulario/js/script.js
+++ Exercises/33-JavaScript-POO-ex/validacao-formulario/js/script.js
@@ -1,44 +1,105 @@
-class ValidForm {
+class ValidateForm {
     constructor() {
-        this.form = document.querySelector('.form');
+        this.form = document.querySelector('#form');
         this.handleSubmit();
     };
 
     handleSubmit() {
         this.form.addEventListener('submit', (e) => {
             e.preventDefault();
-            const isValid = this.validDataFields();
+            const isValid = this.validateDataInputs();
+
+            if (isValid) {
+                alert('Formulário Enviado!');
+                this.form.submit();
+            };
         });
     };
 
-    validDataFields() {
+    validateDataInputs() {
         let isValid = true;
+        this.removeErrors();
 
-        for (const dataField of this.form.querySelectorAll('.data-field')) {
-            const label = dataField.previousElementSibling.innerText.replace(':', '');
+        for (const dataInput of this.form.querySelectorAll('input.validate')) {
+            const label = dataInput.previousElementSibling.innerText.replace(':', '');
 
-            if (!dataField.value) {
-                this.createError(dataField, `O campo "${label}" não pode estar vazio!`);
+            if (!dataInput.value) {
+                this.returnsError(dataInput, `O campo "${label}" não pode estar vazio!`);
                 isValid = false;
             };
 
-            if (dataField.id === 'cpf') {
-                const validaCpf = new ValidaCpf(dataField.value);
-                if (!validaCpf.iniciarValidacao()) {
-                    this.createError(dataField, 'CPF Inválido!');
-                    isValid = false;
-                };
-            };
+            if (dataInput.id === 'cpf' && !this.validateCpf(dataInput)) isValid = false;
+
+            if (dataInput.id === 'usuario' && !this.validateUsuario(dataInput)) isValid = false;
         };
+
+        if (!this.validateSenhas()) isValid = false;
+
+        return isValid;
     };
 
-    createError(dataField, errorMsg) {
-        
+    validateCpf(cpfInput) {
+        const validateCpf = new ValidaCpf(cpfInput.value);
+
+        if (!validateCpf.iniciarValidacao()) {
+            this.returnsError(cpfInput, 'CPF Inválido!');
+            return false;
+        };
+
+        return true;
+    };
+
+    validateUsuario(usuarioInput) {
+        const usuario = usuarioInput.value;
+        let isValid = true;
+
+        if (usuario.length < 3 || usuario.length > 12) {
+            this.returnsError(usuarioInput, 'O usuário deve ter entre 3 a 12 caracteres!');
+            isValid = false;
+        };
+
+        if (!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+            this.returnsError(usuarioInput, 'O usuário deve conter apenas letras e/ou números!');
+            isValid = false;
+        };
+
+        return isValid;
+    };
+
+    validateSenhas() {
+        const senhaInput = this.form.querySelector('input#senha');
+        const repetirSenhaInput = this.form.querySelector('input#repetir-senha');
+        const senha = senhaInput.value;
+        const repetirSenha = repetirSenhaInput.value;
+
+        let isValue = true;
+
+        if (senha.length < 6 || senha.length > 12) {
+            this.returnsError(senhaInput, 'A senha deve conter entre 6 a 12 caracteres!');
+            isValue = false;
+        };
+
+        if (repetirSenha !== senha) {
+            this.returnsError(senhaInput, 'As senhas não coincidem!');
+            this.returnsError(repetirSenhaInput, 'As senhas não coincidem!');
+            isValid = false;
+        };
+
+        return isValue;
+    };
+
+    returnsError(dataInput, errorMsg) {
         const p = document.createElement('p');
-        p.classList.add('error-text');
+        p.classList.add('error');
         p.innerText = errorMsg;
-        dataField.insertAdjacentElement('afterend', p);
+        dataInput.insertAdjacentElement('afterend', p);
+    };
+
+    removeErrors() {
+        for (const pError of this.form.querySelectorAll('p.error')) {
+            pError.remove();
+        };
     };
 };
 
-const validForm = new ValidForm();
+const validateForm = new ValidateForm();
