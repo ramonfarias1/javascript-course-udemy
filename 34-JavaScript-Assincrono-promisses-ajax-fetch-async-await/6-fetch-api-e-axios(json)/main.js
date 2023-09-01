10ac1df0413bab79dca10beacae985047e95c48b
Ramon Farias <114533455+ramonfarias1@users.noreply.github.com>
2023-09-01 17:34:47 -0300

    update

--- 34-JavaScript-Assincrono-promisses-ajax-fetch-async-await/6-fetch-api-e-axios(json)/main.js
+++ 34-JavaScript-Assincrono-promisses-ajax-fetch-async-await/6-fetch-api-e-axios(json)/main.js
@@ -1,16 +1,23 @@
-fetch('pessoas.json')
-    .then ( response => response.json() )
-    .then ( pessoas => criaTabela(pessoas) );
+// fetch('pessoas.json')
+//     .then ( response => response.json() )
+//     .then ( pessoas => criaTabela(pessoas) );
+
+// axios.get('pessoas.json')
+//     .then( response => criaTabela(response.data));
+
+(async function() {
+    const response = await axios.get('pessoas.json');
+    criaTabela(response.data);
+}());
 
 function criaTabela(pessoas) {
     const table = document.createElement('table');
     const trHeaders = document.createElement('tr');
-    const chaves = [...Object.keys(pessoas[0])];
-    console.log(chaves);
+    const chavesObjetoPessoas = Object.keys(pessoas[0]);
 
-    for (const chave of chaves) {
+    for (const chave of chavesObjetoPessoas) {
         const th = document.createElement('th');
-        th.innerHTML = chave;
+        th.innerHTML = chave.toUpperCase();
         trHeaders.appendChild(th);
     };
 
@@ -19,7 +26,7 @@ function criaTabela(pessoas) {
     for (const pessoa of pessoas) {
         const tr = document.createElement('tr');
 
-        for (const chave of chaves) {
+        for (const chave of chavesObjetoPessoas) {
             const td = document.createElement('td');
             td.innerHTML = pessoa[chave];
             tr.appendChild(td);
